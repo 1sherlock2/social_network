b2f5739ce1b2bb8ea25a50e4804ab395bf913b9f
a.i.gazdiev <a.i.gazdiev@tmn3.etagi.com>
2021-01-13 00:15:48 +0500

    other_dev

--- server/controllers/EntryController.js
+++ server/controllers/EntryController.js
@@ -1,20 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 const rooms = require("../roomData");
+const bcrypt = require('bcrypt');
 
 router.post("/", (req, res) => {
   try {
     const { roomName, roomPass } = req.body;
-    const hasRoom = !rooms.has(roomName)
-
-    console.log(rooms)
-    console.log(roomName, rooms.has(roomName))
+    const hasRoom = rooms.has(roomName)
     if (!hasRoom) {
       res.status(404).json({status: false, message: 'Данной комнаты не существует. Введите корректное название или создайте свою комнату'})
     } else {
       const getRoom = rooms.get(roomName);
       const entriesRoom = [...getRoom.entries()];
-      
+      const comparePass = entriesRoom.filter(items => items[0] === 'password')[0].reduce((acc, el) => acc[el] = el, {})    
+      if (!bcrypt.compareSync(roomPass, comparePass )) {
+        res.status(404).json({status: false, message: 'Авторизационные данные неверны' })
+      }
       res.status(200).json(entriesRoom);
     }
   } catch (e) {
